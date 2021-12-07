5e1175a4ef50b2784905393d18b743d63d775869
bazry <barry_jc_ryan@yahoo.co.uk>
2021-12-07 22:56:25 +0000

    parse user input

--- src/main/java/com/lme/martian/MartianRobotsApplication.java
+++ src/main/java/com/lme/martian/MartianRobotsApplication.java
@@ -1,9 +1,9 @@
 package com.lme.martian;
 
 public class MartianRobotsApplication {
-
+	
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		new UserInputHandler();
 	}
-
+	
 }
